13d2226d9ab18d14d7f1e874da1b852c2c0f9b41
qq3 <kov_nikita2003@mail.ru>
2022-08-09 20:10:29 +0600

    second

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import { Cell } from '../models/Cell';
 
 interface CellProps{
-	cell: Cell
+	cell: Cell;
+	selected: boolean;
+	click: (cell: Cell) => void
 }
 
 
-const  CellComponent: React.FC<CellProps> = ({cell}) => {
+const  CellComponent: React.FC<CellProps> = ({cell, selected, click}) => {
 	return <div
+	onClick={() => click(cell)}
+	className={['cell', cell.color, selected ? 'selected' : ''].join(' ')}
+	style={{background: cell.available && cell.figure ? 'green' : ''}}
+	>
 
-	 className={['cell', cell.color].join(' ')}
-	 >
+	{cell.available && !cell.figure && <div className={"available"}></div>}
+
 		{cell.figure?.logo && <img src={cell.figure.logo} alt=''/>}
+
 	</div>
 }
 
